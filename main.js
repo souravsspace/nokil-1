0583adaa795841b8f4b7b5eca12ae500df79df4b
souravsspace <88476413+souravsspace@users.noreply.github.com>
2022-06-24 14:40:03 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+    // for navbar btn
+
+    const primaryNav = document.querySelector('.primary-navigation');
+    const navToggle = document.querySelector('.mobile-nav-toggle');
+    
+    
+    navToggle.addEventListener('click', () => {
+        const visibility = primaryNav.getAttribute('data-visible');
+    
+        if (visibility === "false") {
+            primaryNav.setAttribute('data-visible', true);
+        } 
+        else {
+            primaryNav.setAttribute('data-visible', false);
+        } 
+    });
